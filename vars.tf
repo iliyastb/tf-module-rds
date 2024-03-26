3c10d4cd7aadbdffbf3de7ccf16ec1d963ea9e38
iliyastb <skiliyas300@gmail.com>
2024-03-26 20:38:05 +0530

    Update

---
+++ vars.tf
@@ -0,0 +1,18 @@
+variable "env" {}
+variable "tags" {}
+
+variable "engine" {}
+variable "engine_version" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "database_name" {
+  default = "dummy"
+}
+
+variable "subnet_ids" {}
+variable "no_of_instances" {}
+variable "instance_class" {}
+
+variable "storage_encrypted" {
+  default = true
+}
